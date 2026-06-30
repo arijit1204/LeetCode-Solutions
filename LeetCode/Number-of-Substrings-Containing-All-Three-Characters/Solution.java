c9477afab2ba88f7dd45d16112211d89da661ae2
Arijit Dutta <duttaarijit1204@gmail.com>
2026-06-30 17:01:23 +0530

    Fix result calculation in numberOfSubstrings method

--- LeetCode/Number-of-Substrings-Containing-All-Three-Characters/Solution.java
+++ LeetCode/Number-of-Substrings-Containing-All-Three-Characters/Solution.java
@@ -9,3 +9,23 @@ result += include('c') # | C |
 result -= exclude('a') #  | B |  + | C |  -  | B ∩ C | 
 result -= exclude('b') #  | A | + | C |  -  | A ∩ C | 
 result -= exclude('c') #  | A | + | B | - | A ∩ B | 
+
+
+class Solution {
+    public int numberOfSubstrings(String s) {
+        int n = s.length();
+        int[] count = new int[3]; 
+        int res = 0;
+        int left = 0;
+
+        for (int right = 0; right < n; right++) {
+            count[s.charAt(right) - 'a']++ ;
+            while (count[0] > 0 && count[1] > 0 && count[2] > 0) {
+                res += n - right; 
+                count[s.charAt(left) - 'a']--;
+                left++;
+            }
+        }
+        return res;
+    }
+}
