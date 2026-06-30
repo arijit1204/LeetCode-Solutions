03c175552d2d45dd1467a4b229eaf63c412228b4
Arijit Dutta <duttaarijit1204@gmail.com>
2026-06-30 16:58:56 +0530

    Add solution for Number of Substrings Containing All Three Characters

---
+++ LeetCode/Number-of-Substrings-Containing-All-Three-Characters/Solution.java
@@ -0,0 +1,11 @@
+# | A U B U C | -  | A | - | B | - | C | + | B ∩ C | + | A ∩ C | + | A ∩ B |
+
+result =( n * (n + 1) ) // 2 #  | A U B U C | 
+
+result += include('a') # | A | 
+result += include('b') # | B | 
+result += include('c') # | C | 
+        
+result -= exclude('a') #  | B |  + | C |  -  | B ∩ C | 
+result -= exclude('b') #  | A | + | C |  -  | A ∩ C | 
+result -= exclude('c') #  | A | + | B | - | A ∩ B | 
